c708a77103ea5931377460b3c348805070dc325e
SGSagar29 <160297361+SGSagar29@users.noreply.github.com>
2024-06-25 11:58:18 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.47.0"
+    }
+  }
+}
+
+
+provider "aws"{
+region="ap-south-1"
+}
+
+resource "aws_instance" "one"{
+ami="ami-05e00961530ae1b55"
+instance_type="t2.micro"
+key_name="NewKeyPair"
+tags = {
+Name="Sagar-server"
+}
+}
